291c507f1bba6407059ff04479d5f037fe381495
TheODDYSEY <mosessabz07@gmail.com>
2024-03-11 17:45:17 +0300

    Finished full code

--- controllers/index.js
+++ controllers/index.js
@@ -19,21 +19,20 @@ export const history = [];
  * @returns {Promise} - A promise that resolves when the response is sent.
  */
 export const generateResponse = async (req, res) => {
-    try {
-      const { prompt } = req.body;
-  
-      const result = await model.generateContent(prompt);
-      const response = await result.response;
-      const text = response.text();
-      console.log(text);
-  
-      history.push(text);
-      console.log(history);
-  
-      res.send({ response: text });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  };
-  
+  try {
+    const { prompt } = req.body;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+    history.push(text);
+    console.log(history);
+
+    res.send({ response: text });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { generateResponse, history } = require("./controllers/index.js");
 const dotenv = require("dotenv");
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/generate", (req, res) => {
-  res.send(history);
-});
-
-const bodyParser = require("body-parser");
-const { generateResponse } = require("./controllers/index.js");
-
-//middleware to parse the body content to JSON
+// Middleware to parse the body content to JSON
 app.use(bodyParser.json());
 
+// Route to generate response
 app.post("/generate", generateResponse);
+
+// Route to check response history
+app.get("/history", (req, res) => {
+  res.send(history);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
