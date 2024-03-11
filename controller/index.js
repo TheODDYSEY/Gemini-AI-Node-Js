286f7381d18334ba9bb1c61eb98a8944b7c62ef3
TheODDYSEY <mosessabz07@gmail.com>
2024-03-11 16:50:30 +0300

    generate function 🛠️

--- controller/index.js
+++ controller/index.js
@@ -11,3 +11,29 @@ const modelId = "gemini-pro";
 const model = configuration.getGenerativeModel({ model: modelId });
 
 export const history = [];
+
+/**
+ * Generates a response based on the given prompt.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise} - A promise that resolves when the response is sent.
+ */
+export const generateResponse = async (req, res) => {
+    try {
+      const { prompt } = req.body;
+  
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+  
+      history.push(text);
+      console.log(history);
+  
+      res.send({ response: text });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  
