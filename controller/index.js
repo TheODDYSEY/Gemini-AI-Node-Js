3173b4a19006e146b6d845ee0681b661e85964a4
TheODDYSEY <mosessabz07@gmail.com>
2024-03-11 16:49:17 +0300

    Config gemini ai

---
+++ controller/index.js
@@ -0,0 +1,12 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// GoogleGenerativeAI required config
+const configuration = new GoogleGenerativeAI(process.env.API_KEY);
+
+// Model initialization
+const modelId = "gemini-pro";
+const model = configuration.getGenerativeModel({ model: modelId });
+
--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const port = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+const bodyParser = require("body-parser");
+const { generateResponse } = require("./controllers/index.js");
+
+//middleware to parse the body content to JSON
+app.use(bodyParser.json());
+
+app.post("/generate", generateResponse);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
